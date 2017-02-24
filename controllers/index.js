e0b08a3404366a0d375d6b981031ef431d468a1d
christian antonio valencia guevara <mbernedo5@gmail.com>
2017-02-24 01:27:10 -0500

    test

--- controllers/index.js
+++ controllers/index.js
@@ -8,22 +8,6 @@ var db = mongojs(dblink, ['itesis']);
 
 function login(req, res) {
     let user = req.body;
-    /*db.itesis.update({ _id: mongojs.ObjectId('588b8369734d1d20b6680265') },
-        {
-            $push: {
-                alumnos: {
-                    cod: "12345678",
-                    pwd: "12345678"
-                }
-            }
-        }, function (err, result) {
-            if (err) {
-                res.send(err);
-            } else {
-                res.send(result);
-            }
-        });
-    */
     db.itesis.aggregate(
         { $unwind: '$usuarios' },
         { $match: { 'usuarios.cod': user.cod, 'usuarios.pwd': user.pwd } },
@@ -59,11 +43,6 @@ function login(req, res) {
                 }
             }
         });
-    /*if (user.user && user.pwd) {
-       res.status(200).send({ token: auth.createToken(user), name: "Christian" });
-   } else {
-       res.status(200).send({ cod: 2, msg: "Completar datos" });
-   }*/
 }
 
 function getTesis(req, res) {
